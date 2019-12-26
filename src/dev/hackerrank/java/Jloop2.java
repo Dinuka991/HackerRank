a52a32d5d94f5ceb5a439e9eb7f42951a7a062fc
dev.darshana991 <dev.darshana92@gmail.com>
2019-12-27 04:14:40 +0530

    first

---
+++ src/dev/hackerrank/java/Jloop2.java
@@ -0,0 +1,24 @@
+package dev.hackerrank.java;
+
+public class Jloop2 {
+
+   public static void main(String[] args) {
+	   
+	   
+	   int q = 200;
+	   int a = 23 , b = 50 ;
+	   int n = 15;
+	   
+	   
+	   for(int i=0 ; i<500 ; i ++ ) {
+		   
+		   
+		   
+		   
+		   
+	   }
+	   System.out.println("Hello world");
+   }
+	
+	
+}
